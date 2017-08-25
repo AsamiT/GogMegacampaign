4758cc996c2b1c1b92e68a1001711268202cca83
Asami Tachibana <atachibana2612@gmail.com>
2017-08-24 21:11:06 -0400

    Added a script to automate copying the mod over.

---
+++ copy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This will copy the latest version of Gog's Megacampaign to your mod folder, Asami."
+echo
+echo "Deleting the original mod..."
+rm -r "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
+rm "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
+echo "Copying mod files..."
+cp -r /mnt/f/Desktop/GogMegacampaign/Gogmod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
+echo "Copying mod metadata..."
+cp /mnt/f/Desktop/GogMegacampaign/Gogmod.mod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
+echo "Done!"
+echo "Program complete!"
