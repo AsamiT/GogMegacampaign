d92acf1358854813905e67ede00995dee56de3ea
Asami Tachibana <atachibana2612@gmail.com>
2017-08-26 20:49:35 -0400

    Version 0.5 released!

--- copy.sh
+++ copy.sh
@@ -2,14 +2,10 @@
 
 echo "This will copy the latest version of Gog's Megacampaign to your mod folder, Asami."
 echo
-echo "Deleting the original mod..."
-rm -r "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
+read -rsp $'Press any key to begin.\n' -n1 key
+rm -rv "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
 rm "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
-echo "Deleted."
-echo "Copying mod files..."
-cp -r /mnt/f/Desktop/GogMegacampaign/Gogmod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
-echo "Done!"
-echo "Copying mod metadata..."
-cp /mnt/f/Desktop/GogMegacampaign/Gogmod.mod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
-echo "Done!"
-echo "Program complete!"
+cp -rv /mnt/f/Desktop/GogMegacampaign/Gogmod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
+cp -v /mnt/f/Desktop/GogMegacampaign/Gogmod.mod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
+echo
+echo "Copying completed. Mod is ready for use."
