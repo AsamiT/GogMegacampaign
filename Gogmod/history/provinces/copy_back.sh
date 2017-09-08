1901ddab73631faed8cdf0e3ca5cb967ad1ad3fe
Asami Tachibana <atachibana2612@gmail.com>
2017-09-07 21:47:17 -0400

    Version 0.8 released

---
+++ Gogmod/history/provinces/copy_back.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -v * /mnt/f/Desktop/GogMegacampaign/Gogmod/history/provinces/
---
+++ Gogmod/history/provinces/purge.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rfv *.csv
+rm -rfv *.yml
+rm -rfv *.txt #this purges the directory of EU4-related files.
