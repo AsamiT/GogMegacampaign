8272107d3296a8cafd087d936f34690e87b36186
Asami Tachibana <atachibana2612@gmail.com>
2017-10-03 19:56:33 -0400

    Minor revisions to Protestantism and to provincial religion.

--- win_copy.sh
+++ win_copy.sh
@@ -5,7 +5,7 @@ echo
 read -rsp $'Press any key to begin.\n' -n1 key
 rm -rv "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
 rm "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
-cp -rv /mnt/f/Desktop/GogMegacampaign/Gogmod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
-cp -v /mnt/f/Desktop/GogMegacampaign/Gogmod.mod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
+cp -rv ./Gogmod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
+cp -v ./Gogmod.mod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
 echo
 echo "Copying completed. Mod is ready for use."
