89dcd4d9d39f8239b4c2842f3b8b10bfe1e74e15
Asami Tachibana <atachibana2612@gmail.com>
2017-11-14 19:40:11 -0500

    Scripting changes for local use.

--- win_copy.sh
+++ win_copy.sh
@@ -3,9 +3,9 @@
 echo "This will copy the latest version of Gog's Megacampaign to your mod folder, Asami."
 echo
 read -rsp $'Press any key to begin.\n' -n1 key
-rm -rv "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
-rm "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
-cp -rv ./Gogmod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
-cp -v ./Gogmod.mod "/mnt/d/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
+rm -rv "~/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
+rm "~/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
+cp -rv ./Gogmod "~/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod"
+cp -v ./Gogmod.mod "~/Documents/Paradox Interactive/Europa Universalis IV/mod/Gogmod.mod"
 echo
 echo "Copying completed. Mod is ready for use."
