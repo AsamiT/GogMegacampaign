81067333337fb071973e3607cd5f699ab0900c8e
Asami Tachibana <atachibana2612@gmail.com>
2018-01-04 12:53:56 -0500

    Added by auto-script

--- linux_copy.sh
+++ linux_copy.sh
@@ -5,7 +5,7 @@ echo
 read -rsp $'Press any key to begin.\n' -n1 key
 rm -rv ~/.local/share/Paradox\ Interactive/Europa\ Universalis\ IV/mod/Gogmod
 rm ~/.local/share/Paradox\ Interactive/Europa\ Universalis\ IV/mod/Gogmod.mod
-cp -rv ~/GogMegacampaign/Gogmod ~/.local/share/Paradox\ Interactive/Europa\ Universalis\ IV/mod/Gogmod
-cp -v ~/GogMegacampaign/Gogmod.mod ~/.local/share/Paradox\ Interactive/Europa\ Universalis\ IV/mod/Gogmod.mod
+cp -rv Gogmod ~/.local/share/Paradox\ Interactive/Europa\ Universalis\ IV/mod/Gogmod
+cp -v Gogmod.mod ~/.local/share/Paradox\ Interactive/Europa\ Universalis\ IV/mod/Gogmod.mod
 echo
 echo "Copying completed. Mod is ready for use."
